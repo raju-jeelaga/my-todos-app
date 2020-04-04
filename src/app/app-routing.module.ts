e1bef22af2a05b2b03b19265399dfef8a448ebe8
Raju Jeelaga <rajujeelaga@gmail.com>
2020-04-04 18:33:23 +0530

    todos list page date is showing through API key
    todos list page date is showing through API key

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { TodosListComponent } from './todo/todos-list/todos-list.component';
 import { AddEditTodoComponent } from './todo/add-edit-todo/add-edit-todo.component';
@@ -15,7 +16,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    HttpClientModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/todo/todos-list/todos-list.component.ts
+++ src/app/todo/todos-list/todos-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-todos-list',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosListComponent implements OnInit {
 
-  constructor() { }
+  todos: any[] = []; 
+  isLoading: boolean = false;
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.http.get('https://todos-api-dev.herokuapp.com/todos').subscribe((data:any[] ) => {
+    this.isLoading = false; 
+    this.todos = data;
+    console.log('Data Received', this.todos);
+    });
   }
 
 }
